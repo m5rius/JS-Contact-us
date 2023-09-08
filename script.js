2bab35beba4e9cf83c534355aa92c75c487b4593
m5rius <marius585mv@gmail.com>
2023-09-08 14:20:05 +0300

    Functions added

--- script.js
+++ script.js
@@ -1,241 +1,114 @@
-let containerDiv = document.querySelector('div.container')
+let containerDiv = document.querySelector('.container')
 
-let wrapDiv = document.createElement('div')
-wrapDiv.className = 'wrap'
-
+function createWrapDiv(){
+    let wrapDiv = document.createElement('div')
+    wrapDiv.className = 'wrap'
+    wrapDiv.style =`display: grid;
+                    grid-template-columns: 1fr 1fr;
+                    gap: 20px;`
+    return wrapDiv
+}
+let wrapDiv = createWrapDiv()
 containerDiv.append(wrapDiv)
 
-let box1Div = document.createElement('div')
-box1Div.className = "box1"
-let box2Div = document.createElement('div')
-box2Div.className = "box2"
+function createBox1(){
+    let box1Div = document.createElement('div')
+    box1Div.className = "box1"
+    return box1Div
+}
+
+function createBox2(){
+    let box2Div = document.createElement('div')
+    box2Div.className = "box2"
+    return box2Div
+}
+
+let box1Div = createBox1()
+let box2Div = createBox2()
 wrapDiv.append(box1Div, box2Div)
 
-let headingH1 = document.createElement('h1')
-headingH1.textContent ="Find Us"
-headingH1.className = "heading"
+function createHeadingH1() {
+    let headingH1 = document.createElement('h1')
+    headingH1.textContent ="Find Us"
+    headingH1.className = "heading"
+    headingH1.style =`text-transform: capitalize;`
+    return headingH1
+}
+let headingH1 = createHeadingH1()
 box1Div.append(headingH1)
 
-let boxesDiv = document.createElement('div')
-boxesDiv.className = 'inner-boxes'
+function createInnerBoxes(){
+    let boxesDiv = document.createElement('div')
+    boxesDiv.className = 'inner-boxes'
+    boxesDiv.style =`display: grid;
+                     grid-template-columns: 1fr 1fr;
+                     gap: 15px;`
+    return boxesDiv
+}
+let boxesDiv = createInnerBoxes()
 box1Div.append(boxesDiv)
 
-let innerBoxDiv1 = document.createElement('div')
-innerBoxDiv1.className = "inner-box"
-boxesDiv.append(innerBoxDiv1)
 
-let boxNameH2 = document.createElement('h2')
-boxNameH2.className = 'box-name'
-boxNameH2.textContent = 'Maxima'
-innerBoxDiv1.append(boxNameH2)
+function createInnerBox(title, phoneNumber, email, address){
+    let innerBoxDiv1 = document.createElement('div')
+    innerBoxDiv1.className = "inner-box"
+    boxesDiv.append(innerBoxDiv1)
+    
+    let boxNameH2 = document.createElement('h2')
+    boxNameH2.className = 'box-name'
+    boxNameH2.textContent = title
+    innerBoxDiv1.append(boxNameH2)
+    
+    let boxTextDiv = document.createElement('div')
+    boxTextDiv.className = 'box-text'
+    innerBoxDiv1.append(boxTextDiv)
+    
+    let phoneDiv = document.createElement('div')
+    phoneDiv.className = 'phone'
+    boxTextDiv.append(phoneDiv)
+    
+    let phoneSpan = document.createElement('span')
+    phoneSpan.textContent = 'Phone: '
+    phoneDiv.append(phoneSpan)
+    
+    let phoneTel = document.createElement('a')
+    phoneTel.textContent = phoneNumber
+    phoneTel.href =`tel: ${phoneNumber}`
+    phoneDiv.append(phoneTel)
+    
+    let emailDiv = document.createElement('div')
+    emailDiv.className = 'email'
+    boxTextDiv.append(emailDiv)
+    
+    let emailSpan = document.createElement('span')
+    emailSpan.textContent = 'Email: '
+    emailDiv.append(emailSpan)
+    
+    let emailHref = document.createElement('a')
+    emailHref.textContent = email
+    emailHref.href =`mailto: ${email}`
+    emailDiv.append(emailHref)
+    
+    let addressDiv = document.createElement('div')
+    addressDiv.className = 'address'
+    boxTextDiv.append(addressDiv)
+    
+    let addressSpan = document.createElement('span')
+    addressSpan.textContent = 'Address: '
+    addressDiv.append(addressSpan)
+    
+    let addressHref = document.createElement('a')
+    addressHref.href = `${address}`
+    addressHref.textContent = address
+    addressDiv.append(addressHref)
 
-let boxTextDiv = document.createElement('div')
-boxTextDiv.className = 'box-text'
-innerBoxDiv1.append(boxTextDiv)
+    return createInnerBox
+}
 
-let phoneDiv = document.createElement('div')
-phoneDiv.className = 'phone'
-boxTextDiv.append(phoneDiv)
-
-let phoneSpan = document.createElement('span')
-phoneSpan.textContent = 'Phone: '
-phoneDiv.append(phoneSpan)
-
-let phoneTel = document.createElement('a')
-phoneTel.textContent = 'tel:+370666666'
-phoneTel.href ='tel:+370666666'
-phoneDiv.append(phoneTel)
-
-let emailDiv = document.createElement('div')
-emailDiv.className = 'email'
-boxTextDiv.append(emailDiv)
-
-let emailSpan = document.createElement('span')
-emailSpan.textContent = 'Email: '
-emailDiv.append(emailSpan)
-
-let emailHref = document.createElement('a')
-emailHref.textContent = 'shop1@parduotuve.lt'
-emailHref.href ='mailto:shop1@parduotuve.lt'
-emailDiv.append(emailHref)
-
-let addressDiv = document.createElement('div')
-addressDiv.className = 'address'
-boxTextDiv.append(addressDiv)
-
-let addressSpan = document.createElement('span')
-addressSpan.textContent = 'Address: '
-addressDiv.append(addressSpan)
-
-let addressHref = document.createElement('a')
-addressHref.href = 'address'
-addressHref.textContent = 'Rotušės aikštė 1, Kaunas'
-addressDiv.append(addressHref)
-
-
-///////
-
-let innerBoxDiv2 = document.createElement('div')
-innerBoxDiv2.className = "inner-box"
-boxesDiv.append(innerBoxDiv2)
-
-let boxNameH22 = document.createElement('h2')
-boxNameH22.className = 'box-name'
-boxNameH22.textContent = 'Maxima'
-innerBoxDiv2.append(boxNameH22)
-
-let boxTextDiv2 = document.createElement('div')
-boxTextDiv2.className = 'box-text'
-innerBoxDiv2.append(boxTextDiv2)
-
-let phoneDiv2 = document.createElement('div')
-phoneDiv2.className = 'phone'
-boxTextDiv2.append(phoneDiv2)
-
-let phoneSpan2 = document.createElement('span')
-phoneSpan2.textContent = 'Phone: '
-phoneDiv2.append(phoneSpan2)
-
-let phoneTel2 = document.createElement('a')
-phoneTel2.textContent = 'tel:+370666666'
-phoneTel2.href ='tel:+370666666'
-phoneDiv2.append(phoneTel2)
-
-let emailDiv2 = document.createElement('div')
-emailDiv2.className = 'email'
-boxTextDiv2.append(emailDiv2)
-
-let emailSpan2 = document.createElement('span')
-emailSpan2.textContent = 'Email: '
-emailDiv2.append(emailSpan2)
-
-let emailHref2 = document.createElement('a')
-emailHref2.textContent = 'shop1@parduotuve.lt'
-emailHref2.href ='mailto:shop1@parduotuve.lt'
-emailDiv2.append(emailHref2)
-
-let addressDiv2 = document.createElement('div')
-addressDiv2.className = 'address'
-boxTextDiv2.append(addressDiv2)
-
-let addressSpan2 = document.createElement('span')
-addressSpan2.textContent = 'Address: '
-addressDiv2.append(addressSpan2)
-
-let addressHref2 = document.createElement('a')
-addressHref2.href = 'address'
-addressHref2.textContent = 'Rotušės aikštė 1, Kaunas'
-addressDiv2.append(addressHref2)
-
-////
-
-let innerBoxDiv3 = document.createElement('div')
-innerBoxDiv3.className = "inner-box"
-boxesDiv.append(innerBoxDiv3)
-
-let boxNameH23 = document.createElement('h2')
-boxNameH23.className = 'box-name'
-boxNameH23.textContent = 'Maxima'
-innerBoxDiv3.append(boxNameH23)
-
-let boxTextDiv3 = document.createElement('div')
-boxTextDiv3.className = 'box-text'
-innerBoxDiv3.append(boxTextDiv3)
-
-let phoneDiv3 = document.createElement('div')
-phoneDiv3.className = 'phone'
-boxTextDiv3.append(phoneDiv3)
-
-let phoneSpan3 = document.createElement('span')
-phoneSpan3.textContent = 'Phone: '
-phoneDiv3.append(phoneSpan3)
-
-let phoneTel3 = document.createElement('a')
-phoneTel3.textContent = 'tel:+370666666'
-phoneTel3.href ='tel:+370666666'
-phoneDiv3.append(phoneTel3)
-
-let emailDiv3 = document.createElement('div')
-emailDiv3.className = 'email'
-boxTextDiv3.append(emailDiv3)
-
-let emailSpan3 = document.createElement('span')
-emailSpan3.textContent = 'Email: '
-emailDiv3.append(emailSpan3)
-
-let emailHref3 = document.createElement('a')
-emailHref3.textContent = 'shop1@parduotuve.lt'
-emailHref3.href ='mailto:shop1@parduotuve.lt'
-emailDiv3.append(emailHref3)
-
-let addressDiv3 = document.createElement('div')
-addressDiv3.className = 'address'
-boxTextDiv3.append(addressDiv3)
-
-let addressSpan3 = document.createElement('span')
-addressSpan3.textContent = 'Address: '
-addressDiv3.append(addressSpan3)
-
-let addressHref3 = document.createElement('a')
-addressHref3.href = 'address'
-addressHref3.textContent = 'Rotušės aikštė 1, Kaunas'
-addressDiv3.append(addressHref3)
-
-///
-
-let innerBoxDiv4 = document.createElement('div')
-innerBoxDiv4.className = "inner-box"
-boxesDiv.append(innerBoxDiv4)
-
-let boxNameH24 = document.createElement('h2')
-boxNameH24.className = 'box-name'
-boxNameH24.textContent = 'Maxima'
-innerBoxDiv4.append(boxNameH24)
-
-let boxTextDiv4 = document.createElement('div')
-boxTextDiv4.className = 'box-text'
-innerBoxDiv4.append(boxTextDiv4)
-
-let phoneDiv4 = document.createElement('div')
-phoneDiv4.className = 'phone'
-boxTextDiv4.append(phoneDiv4)
-
-let phoneSpan4 = document.createElement('span')
-phoneSpan4.textContent = 'Phone: '
-phoneDiv4.append(phoneSpan4)
-
-let phoneTel4 = document.createElement('a')
-phoneTel4.textContent = 'tel:+370666666'
-phoneTel4.href ='tel:+370666666'
-phoneDiv4.append(phoneTel4)
-
-let emailDiv4 = document.createElement('div')
-emailDiv4.className = 'email'
-boxTextDiv4.append(emailDiv4)
-
-let emailSpan4 = document.createElement('span')
-emailSpan4.textContent = 'Email: '
-emailDiv4.append(emailSpan4)
-
-let emailHref4 = document.createElement('a')
-emailHref4.textContent = 'shop1@parduotuve.lt'
-emailHref4.href ='mailto:shop1@parduotuve.lt'
-emailDiv4.append(emailHref4)
-
-let addressDiv4 = document.createElement('div')
-addressDiv4.className = 'address'
-boxTextDiv4.append(addressDiv4)
-
-let addressSpan4 = document.createElement('span')
-addressSpan4.textContent = 'Address: '
-addressDiv4.append(addressSpan4)
-
-let addressHref4 = document.createElement('a')
-addressHref4.href = 'address'
-addressHref4.textContent = 'Rotušės aikštė 1, Kaunas'
-addressDiv4.append(addressHref4)
-
-///
+let innerBox1 = createInnerBox('Parduotuvė 1', '+37045461211', 'info@parduotuve1.lt', 'Rotušės aikštė 1, Kaunas')
+let innerBox2 = createInnerBox('Parduotuvė 2', '+37045461212', 'info@parduotuve2.lt', 'Rotušės aikštė 2, Kaunas')
+let innerBox3 = createInnerBox('Parduotuvė 3', '+37045461213', 'info@parduotuve3.lt', 'Rotušės aikštė 3, Kaunas')
+let innerBox4 = createInnerBox('Parduotuvė 4', '+37045461214', 'info@parduotuve4.lt', 'Rotušės aikštė 4, Kaunas')
 
 let iframe = document.createElement('iframe')
 iframe.src = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2294.3423075885275!2d23.887974!3d54.896913899999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46e7220ff3184881%3A0x8002872ac9bbdb7c!2sRotu%C5%A1%C4%97s%20a.%201%2C%2044280%20Kaunas!5e0!3m2!1sen!2slt!4v1692877269499!5m2!1sen!2slt'
